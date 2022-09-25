e648a87ad34c77f2eafa78cce1a62617300676b8
YuehuaYin <112977288+YuehuaYin@users.noreply.github.com>
2022-09-26 00:36:54 +0100

    V1.1 - fixed major error
    Was stupid and forgot to change string input to integers which meant it didn't sort properly.... woops
    Also added more comments

--- sorting calculator.py
+++ sorting calculator.py
@@ -1,11 +1,15 @@
-from re import T
+#Yuehua Yin - September 2022
+#This is a sorting caluclator I am making in python using tkinter. This is my first proper project using tkinter aside from following tutorials online and short tests.
+#The user can enter a list of numbers and click a button to select how the numbers should be sorted. Each step of the sorting is displayed along with number total steps.
+#CURRENT SORTING ALGORITHMS AVAILABLE: bubble sort
+
 from tkinter import *
 
 #set up window
 root = Tk()
 
 #label with instructions for entering numbers
-enter_label = Label(root, text = "Enter numbers below seperated by a comma")
+enter_label = Label(root, text = "Enter numbers below seperated by a comma: ")
 enter_label.grid(column = 0, row = 0)
 
 #entry box for user to enter numbers
@@ -32,6 +36,9 @@ def bubble_sort():
 
     #get numbers from entry field
     input = enter.get().split(",")
+    #CHANGE IT INTO INT OMG WHY DID I FORGET ABOUT THAT!!!!!!!!
+    input = list(map(int,input))
+    print(input)
 
     #variables
     global r
@@ -43,10 +50,11 @@ def bubble_sort():
     #once it gets to the end of list it starts again until no more swaps are made
     while swapped:
         swapped = False
-        for i in range (0, n-1):
+        for i in range (0, n - 1):
             if input[i] > input[i + 1]:
                 swapped = True
                 input[i], input[i + 1] = input[i + 1], input[i]
+                print(input)
                 label = Label(root, text = "Swapped " + str(input[i + 1]) + " and " + str(input[i]) + ": " + str(input))
                 label.grid(column = 0, row = r)
                 r = r + 1
