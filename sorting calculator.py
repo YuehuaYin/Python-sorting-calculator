fe077d581337f8e2db1af3e170fb942e37eb8b7b
YuehuaYin <112977288+YuehuaYin@users.noreply.github.com>
2022-09-25 23:39:52 +0100

    V1
    Version 1 of the application. 
    
    Basic UI. Allows user to enter numbers and select 'Bubble sort' button. Sorts numbers using bubble sort and displays each step including total number to steps. Clears existing labels on button click so the user can sort different lists of numbers.

---
+++ sorting calculator.py
@@ -0,0 +1,67 @@
+from re import T
+from tkinter import *
+
+#set up window
+root = Tk()
+
+#label with instructions for entering numbers
+enter_label = Label(root, text = "Enter numbers below seperated by a comma")
+enter_label.grid(column = 0, row = 0)
+
+#entry box for user to enter numbers
+enter = Entry(root, width = 50)
+enter.grid(column = 0, row = 1)
+
+#row number to start displaying calculation labels
+r = 2
+
+#function for clearing labels
+#clears all the labels in column 0 between row 2 and the last value for r
+def clear_labels():
+    for l in root.grid_slaves():
+        if int(l.grid_info()["column"]) == 0:
+            if int(l.grid_info()["row"]) >= 2:
+                if int(l.grid_info()["row"]) <= r:
+                    l.grid_forget()
+
+#bubble sort function
+def bubble_sort():
+
+    #clear previous labels
+    clear_labels()
+
+    #get numbers from entry field
+    input = enter.get().split(",")
+
+    #variables
+    global r
+    r = 2
+    n = len(input)
+    swapped = True
+
+    #iterate through list and swaps numbers if the current number is greater than the next number
+    #once it gets to the end of list it starts again until no more swaps are made
+    while swapped:
+        swapped = False
+        for i in range (0, n-1):
+            if input[i] > input[i + 1]:
+                swapped = True
+                input[i], input[i + 1] = input[i + 1], input[i]
+                label = Label(root, text = "Swapped " + str(input[i + 1]) + " and " + str(input[i]) + ": " + str(input))
+                label.grid(column = 0, row = r)
+                r = r + 1
+
+    #label showing how many steps it took to sort
+    steps_label = Label(root, text = "Total steps: " + str(r - 2))
+    steps_label.grid(column = 0, row = r)
+        
+
+#label for buttons
+button_label = Label(root, text = "Click a button below to sort:")
+button_label.grid(column = 1, row = 0)
+
+#buttons
+myButton = Button(root, text = "Bubble sort", command = bubble_sort)
+myButton.grid(column = 1, row = 1)
+
+root.mainloop()
