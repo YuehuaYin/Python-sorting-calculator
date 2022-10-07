22d93ae70ae15804dd1608ae424bc5d25509b642
YuehuaYin <112977288+YuehuaYin@users.noreply.github.com>
2022-10-08 00:54:25 +0100

    V2.0
    -added insertion sort
    -modified bubble sort so it counts comparisons as steps too
    -added frames to put widgets in for better layout
    -changed labels to scrolled text widget for easier reading
    -changed fonts to make it look nicer

--- sorting calculator.py
+++ sorting calculator.py
@@ -1,48 +1,54 @@
-#Yuehua Yin - September 2022
+#Yuehua Yin 2022
 #This is a sorting caluclator I am making in python using tkinter. This is my first proper project using tkinter aside from following tutorials online and short tests.
 #The user can enter a list of numbers and click a button to select how the numbers should be sorted. Each step of the sorting is displayed along with number total steps.
-#CURRENT SORTING ALGORITHMS AVAILABLE: bubble sort
+#CURRENT SORTING ALGORITHMS AVAILABLE: bubble sort, insertion sort
 
 from tkinter import *
+from tkinter import scrolledtext
 
 #set up window
 root = Tk()
+root.title("Sorting calculator")
+
+#frames for layout
+left_frame = LabelFrame(root)
+left_frame.grid(column = 0, row = 0)
+
+button_frame = LabelFrame(root)
+button_frame.grid(column = 1, row = 0)
 
 #label with instructions for entering numbers
-enter_label = Label(root, text = "Enter numbers below seperated by a comma: ")
-enter_label.grid(column = 0, row = 0)
+enter_label = Label(left_frame, text = "Enter numbers below seperated by a comma: ", font = ("Impact", 22))
+enter_label.grid(row = 0)
 
 #entry box for user to enter numbers
-enter = Entry(root, width = 50)
-enter.grid(column = 0, row = 1)
+enter = Entry(left_frame, width = 50, font = ("Arial", 15))
+enter.grid(row = 1)
 
-#row number to start displaying calculation labels
-r = 2
+#set up scrolltext widget
+scrolltxt = scrolledtext.ScrolledText(left_frame, wrap=WORD, width=53, height=15, font = ("Times New Roman", 15))
+scrolltxt.grid(row = 2)
+scrolltxt.configure(state='disabled')
 
-#function for clearing labels
-#clears all the labels in column 0 between row 2 and the last value for r
-def clear_labels():
-    for l in root.grid_slaves():
-        if int(l.grid_info()["column"]) == 0:
-            if int(l.grid_info()["row"]) >= 2:
-                if int(l.grid_info()["row"]) <= r:
-                    l.grid_forget()
+#set up step count label
+steps_label = Label(left_frame, text = "Step count for sort goes here!", font = ("Arial", 15))
+steps_label.grid(row = 3)
 
 #bubble sort function
 def bubble_sort():
 
-    #clear previous labels
-    clear_labels()
-
+    #delete and enable scrolltxt
+    scrolltxt.configure(state='normal')
+    scrolltxt.delete('1.0', END)
+    
     #get numbers from entry field
     input = enter.get().split(",")
-    #CHANGE IT INTO INT OMG WHY DID I FORGET ABOUT THAT!!!!!!!!
+    #CHANGE IT INTO INT
     input = list(map(int,input))
-    print(input)
 
     #variables
-    global r
-    r = 2
+    global steps_label
+    s = 0
     n = len(input)
     swapped = True
 
@@ -51,25 +57,73 @@ def bubble_sort():
     while swapped:
         swapped = False
         for i in range (0, n - 1):
+            scrolltxt.insert("insert", "Compare " + str(input[i]) + " and " + str(input[i + 1]) + "\n")
+            s = s + 1
             if input[i] > input[i + 1]:
                 swapped = True
                 input[i], input[i + 1] = input[i + 1], input[i]
-                print(input)
-                label = Label(root, text = "Swapped " + str(input[i + 1]) + " and " + str(input[i]) + ": " + str(input))
-                label.grid(column = 0, row = r)
-                r = r + 1
+                #label to display step
+                scrolltxt.insert("insert", "Swap " + str(input[i + 1]) + " and " + str(input[i]) + ": " + str(input) + "\n")
+                s = s + 1
 
     #label showing how many steps it took to sort
-    steps_label = Label(root, text = "Total steps: " + str(r - 2))
-    steps_label.grid(column = 0, row = r)
-        
+    steps_label.grid_forget()
+    steps_label = Label(left_frame, text = "Total steps (comparisons and swaps): " + str(s), font = ("Arial", 15))
+    steps_label.grid(row = 3)
+
+    #disable scrolltxt
+    scrolltxt.configure(state='disabled')
+
+#insertion sort function
+def insertion_sort():
+
+    #clear and enable scrolltxt
+    scrolltxt.configure(state='normal')
+    scrolltxt.delete('1.0', END)
+
+    #get numbers from entry field
+    input = enter.get().split(",")
+    #CHANGE IT INTO INT
+    input = list(map(int,input))
+
+    #variables
+    global steps_label
+    s = 0
+    n = len(input)
+
+    #Insert numbers into sorted array one at a time by comparing each value to sorted array and inserting into correct position
+    for i in range (1, n):
+        key = input[i]
+        j = i - 1
+        while j >= 0 and input[j] > key:
+            scrolltxt.insert("insert", "Compare " + str(key) + " and " + str(input[j]) + "\n")
+            s = s + 1
+            input[j + 1] = input[j]
+            j = j - 1
+        scrolltxt.insert("insert", "Compare " + str(key) + " to " + str(input[j]) + "\n")
+        s = s + 1
+        input[j + 1] = key
+        scrolltxt.insert("insert", "Insert " + str(input[j+1]) + " after " + str(input[j]) + ": " + str(input) + "\n")
+        s = s + 1
+
+    #label showing how many steps it took to sort
+    steps_label.grid_forget()
+    steps_label = Label(left_frame, text = "Total steps (comparisons and insertions): " + str(s), font = ("Arial", 15))
+    steps_label.grid(row = 3)
+
+    #disable scrolltxt
+    scrolltxt.configure(state='disabled')
 
 #label for buttons
-button_label = Label(root, text = "Click a button below to sort:")
-button_label.grid(column = 1, row = 0)
+button_label = Label(button_frame, text = "Select algorithm:", font = ("Impact", 20))
+button_label.grid(row = 0)
 
 #buttons
-myButton = Button(root, text = "Bubble sort", command = bubble_sort)
-myButton.grid(column = 1, row = 1)
+button = Button(button_frame, text = "Bubble sort", font = ("Arial", 16), command = bubble_sort)
+button.grid(row = 1, padx=25, pady=5)
 
+button = Button(button_frame, text = "Insertion sort", font = ("Arial", 16), command = insertion_sort)
+button.grid(row = 2, padx= 25, pady=5)
+
+#run program
 root.mainloop()
